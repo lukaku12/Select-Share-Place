9adb738d4eba719362b643ee1116cb5d1b767fb1
lukakurdadze <lukakurdadze2@gmail.com>
2024-01-23 15:43:35 +0400

    feat: add axios request to get location response from google api

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,29 @@
-"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+define("app", ["require", "exports", "axios"], function (require, exports, axios_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    axios_1 = __importDefault(axios_1);
+    const form = document.querySelector('form');
+    const addressInput = document.getElementById('address');
+    const API_KEY = "AIzaSyA5afr5YylcKIc1rfUDM6CpNHz0uVFolMU";
+    function GET_GOOGLE_API_URL(address, apiKay) {
+        const fixedAddress = encodeURI(address);
+        return `https://maps.googleapis.com/maps/api/geocode/json?address=${fixedAddress}&key=${apiKay}`;
+    }
+    function searchAddressHandler(event) {
+        event.preventDefault();
+        const enteredAddress = addressInput.value;
+        const apiUrl = GET_GOOGLE_API_URL(enteredAddress, API_KEY);
+        axios_1.default.get(apiUrl)
+            .then(r => {
+            console.log(r);
+        })
+            .catch(e => {
+            console.log(e);
+        });
+    }
+    form.addEventListener('submit', searchAddressHandler);
+});
 //# sourceMappingURL=bundle.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,38 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+const API_KEY = "AIzaSyA5afr5YylcKIc1rfUDM6CpNHz0uVFolMU"
+
+type GoogleGeocodingResponse = {
+    result: { geometry: { location: { lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
+
+function GET_GOOGLE_API_URL (address: string, apiKay: string) {
+    const fixedAddress = encodeURI(address)
+    return `https://maps.googleapis.com/maps/api/geocode/json?address=${fixedAddress}&key=${apiKay}`;
+}
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    // send this to Google's API!
+    const apiUrl = GET_GOOGLE_API_URL(enteredAddress, API_KEY);
+
+    axios.get<GoogleGeocodingResponse>(apiUrl)
+         .then(r => {
+             if (r.data.status !== 'OK') {
+                 throw new Error('Could not fetch Location!');
+             }
+             const coordinates = r?.data?.result[0]?.geometry?.location;
+
+         })
+         .catch(e => {
+             alert(e.message);
+             console.log(e)
+         });
+}
+
+form.addEventListener('submit', searchAddressHandler);
